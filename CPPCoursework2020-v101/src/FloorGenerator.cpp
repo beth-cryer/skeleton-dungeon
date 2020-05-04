42173251dcd43e72375ce06ec9db9b4bf60ae551
Ben Cryer <benjamincryer@hotmail.co.uk>
2020-05-04 23:07:00 +0100

    fixed floor generator and started on room gen

--- CPPCoursework2020-v101/src/FloorGenerator.cpp
+++
@@ -1,1 +0,0 @@
-#include "FloorGenerator.h"
--- CPPCoursework2020-v101/src/FloorGenerator.h
+++ CPPCoursework2020-v101/src/FloorGenerator.h
@@ -1,9 +1,61 @@
 #pragma once
 #include "header.h"
+#include "PlayerObject.h"
+
 #include <vector>
 #include <list>
 #include <tuple>
 #include <time.h>
+
+class Room
+{
+public:
+	Room(GameEngine* pEngine, bool room)
+		: pEngine(pEngine),
+		room(room)
+	{
+	}
+
+	void onEnter() {
+		//First, overwrite GameEngine tile managers with this room's managers
+
+
+
+		//Create and fill object array in GameEngine
+		pEngine->drawableObjectsChanged();
+
+		//Destroy any existing objects
+		pEngine->destroyOldObjects(true);
+
+		pEngine->player = new PlayerObject(pEngine, pEngine->GetTilesSolid());
+		pEngine->createObjectArray(100);
+		pEngine->appendObjectToArray(pEngine->player);
+
+		//Add all objects
+		for (auto it = objects.begin(); it != objects.end(); it++) {
+			pEngine->appendObjectToArray(*it);
+		}
+
+		pEngine->setAllObjectsVisible(true);
+	}
+
+	void onExit() {
+
+	}
+
+	//Signifies if the room is valid or null
+	bool room;
+
+	Room* roomRight = nullptr, * roomLeft = nullptr, * roomUp = nullptr, * roomDown = nullptr;
+
+	//A list of objects that the room contains. Placed upon entering the room and destroyed on exit
+	//(when an object in this list is destroyed, it needs to remove itself from the list first or it will be recreated when you exit and enter the room again)
+	std::list<DisplayableObject*> objects;
+
+	//Pointer used to tell the engine to create/destroy objects
+	GameEngine* pEngine;
+
+};
 
 class FloorGenerator
 {
@@ -66,10 +118,16 @@ public:
 
 					valid = true;
 					for (auto it = digNodes.begin(); it != digNodes.end(); it++) { //loop through all existing dig nodes
-						//Making sure dig nodes are at least (sector+1) tiles apart (any smaller risks cutting paths off)
 						//Using Manhattan Distance
-						int dist = std::abs(std::get<0>(*it) - rx) + std::abs(std::get<1>(*it) - ry);
-						if (dist < sector+2) valid = false;
+						int xdist = std::abs(std::get<0>(*it) - rx);
+						int ydist = std::abs(std::get<1>(*it) - ry);
+						int dist = xdist + ydist;
+
+						//Making sure dig nodes are at least (sector+1) tiles apart (any smaller risks cutting paths off)
+						if (dist < sector+1) valid = false;
+
+						//Also don't want nodes to be placed diagonal to another, can cut off passages when we expand the hole
+						if (xdist == ydist) valid = false;
 					}
 
 				//Now it'll repeat until dist >= 3
@@ -105,5 +163,41 @@ public:
 		return floor;
 	}
 
-};
+	std::vector<Room*> genRooms(GameEngine* pEngine, grid floorLayout)
+	{
+		std::vector<std::vector<Room*>> floor;
+		int w, h;
 
+		//We're gonna go through every tile in the floor and create a list of new Room()s, leaving them empty for now
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+				floor[y].push_back(new Room(pEngine, x == 2)); //set new Room to true if it is an allocated room tile
+			}
+		}
+
+		//Then, iterate through the list and connect each to its adjacent rooms, removing any Door tiles that don't lead anywhere
+		for (int y = 0; y < h; y++) {
+			for (int x = 0; x < w; x++) {
+
+				auto currentRoom = floor[y][x];
+				//If this is a filled room:
+				if (currentRoom->room) { 
+					//Check each of the four adjacent Rooms to see if room==true
+					//If so, assign the appropriate room pointer in currentRoom
+					if (floor[y][x + 1]->room) currentRoom->roomRight = floor[y][x + 1];
+					if (floor[y][x - 1]->room) currentRoom->roomLeft = floor[y][x - 1];
+					if (floor[y + 1][x]->room) currentRoom->roomDown = floor[y + 1][x];
+					if (floor[y - 1][x]->room) currentRoom->roomUp = floor[y - 1][x];
+				}
+			}
+		}
+
+		//Next, for each object that has a set amount in each Floor:
+			//pick a valid room at random and, if it has a free corresponding Special tile available, place it there
+
+		//Next place objects that require a set amount per Room: (ie. enemies)
+			//loop through every room and place
+
+	}
+
+};
